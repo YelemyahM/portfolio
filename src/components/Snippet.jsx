6323b0ff6efa6c81a5ae63c850238293c31f99c6
yelemyah.m <yelemyah.muamba@gmail.com>
2024-11-30 16:03:19 +0100

    feat: improved performance by adding  attribute for images

--- src/components/Snippet.jsx
+++ src/components/Snippet.jsx
@@ -2,7 +2,7 @@ import style from "../styles/components/Snippet.module.css";
 export const Snippet = ({ img, alt, description }) => {
   return (
     <div className={style.snippet}>
-      <img src={img} alt={alt} />
+      <img src={img} alt={alt} loading="lazy" />
     </div>
   );
 };
